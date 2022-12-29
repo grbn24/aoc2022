5d58dfc88deff887ac2e447448b3b1a6a805c644
Gerben <grbn24@gmail.com>
2022-12-29 01:08:30 +0100

    Working Day 07

--- 07/main.jl
+++ 07/main.jl
@@ -1,70 +1,39 @@
-mutable struct dir{T<:Number}
-    name::String
-    size::T
-    subdir::Vector{String}
-end
-
 # fname = "07/testdata.txt"
 fname = "07/data.txt"
 
-dirs = Dict{String, dir{Int64}}()
-# println(dirs)
-coms = readlines(fname)
-n_end = length(coms)
-idx = 0
-while idx < n_end
-    global idx += 1
-    l = coms[idx]
-    m = match(r"\$ cd (.*)", l)
-    global curdir
-    if !isnothing(m)
-        global curdir = m.captures[1]
-        if curdir == ".."
-            continue
-        end
-        dirs[curdir] = dir{Int64}(curdir, 0, [])
-        # println(dirs)
-        continue
-    end
-    if l == raw"$ ls"
-        while(idx < n_end && !contains(coms[idx+1], raw"$"))
-            idx +=1
-            l = coms[idx]
-            m = match(r"^([0-9]*) .*", l)
-            if !isnothing(m)
-                dirs[curdir].size += parse(Int64, m.captures[1])
+SZ = Dict{String, Int64}()
+curdir = String[]
+for l in readlines(fname)
+    words = split(l, " ")
+    # println(words)
+    if words[1] == raw"$"
+        # println(words[2])
+        if words[2] == raw"cd"
+            if words[3] == raw".."
+                pop!(curdir)
+                # println(curdir)
                 continue
             end
-            m = match(r"dir (.*)", l)
-            if !isnothing(m)
-                # println(m.captures[1])
-                push!(dirs[curdir].subdir, m.captures[1])
+            push!(curdir, words[3])
+            if !in(keys(SZ), join(curdir))
+                SZ[join(curdir)] = 0
             end
+            # println(curdir)
+            continue
+        end
+        if words[2] == raw"ls"
+            continue
         end
     end
-end
-
-
-function get_total_size(dir, dirs)
-    res = dir.size
-    for sd in dir.subdir
-        # println(dirs)
-        res += get_total_size(dirs[string(sd)], dirs)
+    if words[1] == raw"dir"
+        continue
     end
-    return res
-end
-
-dirsizes = Dict{String, Int64}()
-# for k in keys(dirs)
-#     # println(dirs[k])
-#     dirsizes[k] = get_total_size(dirs[k], dirs)
-# end
-
-res1 = 0
-for (k, v) in dirs
-    if v.size <= 100000
-        global res1 += v.size
+    for i in eachindex(curdir)
+        SZ[join(curdir[1:i])] += parse(Int64, words[1])
     end
 end
-println(dirsizes)
-println(res1)
+
+res1 = sum([x for x in values(SZ) if x < 100000])
+res2 = minimum([x for x in values(SZ) if 70000000 + x - SZ["/"] > 30000000])
+@show res1
+@show res2
