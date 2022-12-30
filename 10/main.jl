5b9e42be22a75752728ee469d8b3e6c3dd17cb79
Gerben <grbn24@gmail.com>
2022-12-30 13:44:07 +0100

    Day10 p2

--- 10/main.jl
+++ 10/main.jl
@@ -5,25 +5,38 @@ fname = "10/data.txt"
 n_cycle = 0
 x = 1
 ss = 0
-
-for l in readlines(fname)
-    if l == "noop"
-        global n_cycle += 1
-        dx = 0
-    else
-        r, inst, dx = @scanf(l, "%s %d", String, Int64)
+in_add = false
+dx_store = 0
+res2 = ""
+open(fname) do f
+    while !eof(f)
+        global in_add
+        global dx_store
+        if in_add == true
+            dx = dx_store
+            in_add = false
+        else
+            l = readline(f)
+            if l != "noop"
+                r, inst, dx_store = @scanf(l, "%s %d", String, Int64)
+                in_add = true
+            end
+            dx = 0
+        end
         global n_cycle += 1
         if mod(n_cycle-20, 40) == 0
             global ss += x*n_cycle
             println("cycle= $(n_cycle), x = $(x), ss = $(ss)")
         end
-        global n_cycle += 1
+        if abs(mod1(n_cycle,40)-1-x) <= 1
+            global res2 *= "#"
+        else
+            global res2 *= "."
+        end
+        global x += dx
     end
-    if mod(n_cycle-20, 40) == 0
-        global ss += x*n_cycle
-        println("cycle= $(n_cycle), x = $(x), ss = $(ss)")
-    end
-    global x += dx
-    # println("cycle= $(n_cycle), x = $(x)")
 end
 println("Signal Strenght = $(ss)")
+for idx = 1:40:201
+    println(res2[idx:(idx+39)])
+end
